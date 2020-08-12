11ab3c916b325fbb06a8700f58c4a31d6ab1c77e
Jepri <55873259+jeffreymas@users.noreply.github.com>
2020-08-12 18:01:48 +0700

    Add files via upload

---
+++ BangunDatarUp/BangunDatarUp/BangunDatar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BangunDatarUp
+{
+    public abstract class BangunDatar
+    {
+        public double Luas, Kel, a, b;
+        public abstract void set_a(double a);
+        public abstract void set_b(double b);
+        public abstract void setLuas();
+        public abstract void setKel();
+        public abstract double get_a();
+        public abstract double get_b();
+        public abstract double getLuas();
+        public abstract double getKel();
+        public abstract void hasil();
+    }
+}
---
+++ BangunDatarUp/BangunDatarUp/Lingkaran.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace BangunDatarUp
+{
+    public class Lingkaran : BangunDatar
+    {
+        protected double Luas, Kel, r;
+
+        public override void set_a(double a)
+        {
+            this.r = a;
+        }
+
+        public override void set_b(double b)
+        {
+            this.r = b;
+        }
+        public override void setKel()
+        {
+            Kel = 2 * r * 3.14;
+        }
+
+        public override void setLuas()
+        {
+            Luas = r * r * 3.14;
+        }
+
+        public override double get_a()
+        {
+            return r;
+        }
+
+        public override double get_b()
+        {
+            return r;
+        }
+        public override double getKel()
+        {
+            return Kel;
+        }
+
+        public override double getLuas()
+        {
+            return Luas;
+        }
+
+        public override void hasil()
+        {
+            try
+            {
+                Console.WriteLine("Lingkaran");
+                Console.WriteLine("Luas = " + r + " * " + r + " * 3.14 = " + Luas);
+                Console.WriteLine("Keliling = " + 2 + " * " + r + " * 3.14 = " + Kel);
+            }
+            catch (ArithmeticException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
